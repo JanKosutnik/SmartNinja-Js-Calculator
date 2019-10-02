f0b5d6eb603408736cdcc14fd46d33c8dd7061e0
Jan Kosutnik <jan.kosutnik@icloud.com>
2019-10-02 19:02:27 +0200

    Second commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function calc()
+{
+    var number1 = parseFloat(document.getElementById('number1').value);
+    var number2 = parseFloat(document.getElementById('number2').value);
+    var operation = document.getElementById('operation').value;
+
+    if(operation ==='+')
+        document.getElementById("result").value=number1+number2;
+
+    if(operation ==='-')
+    document.getElementById("result").value=number1-number2;
+        
+    if(operation ==='*')
+    document.getElementById("result").value=number1*number2;
+    
+    if(operation ==='/')
+    document.getElementById("result").value=number1/number2;
+}
